18c5133b264a8c40af509d3f6ea8b24d7d960fe1
ryanpagano <shutdafuqupandplay@gmail.com>
2017-05-02 11:38:56 -0300

    RPG-Game
    1st project with TDAs

---
+++ TP1 - TDA Basico/Personaje.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1___TDA_Basico
+{
+    class Personaje
+    {
+        public float experiencia;
+        public float vidaMaxima;
+        public float vida;
+        public float manna;
+
+        public void Curacion()
+        {
+            if (vida > vidaMaxima)
+            {
+                vida = vidaMaxima;
+            }
+            else
+            {
+                vida++;
+            }
+        }
+    }
+}
